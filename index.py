197ef1d76cab59e636a20afa1ccdad96d80a7b58
Lucas Bispo <skaliboo1@gmail.com>
2022-12-03 22:28:33 -0300

    create funcion main

---
+++ index.py
@@ -0,0 +1,30 @@
+import sys
+
+from pytube import YouTube
+
+from banner import print_banner
+
+
+def main():
+    try:
+        if sys.argv[1] and sys.argv[1] == "-path":
+            print("initiating download")
+            with open(sys.argv[2], "r") as file:
+                for i, line in enumerate(file):
+                    yt = YouTube(line)
+                    print(f" {i} - Downloading {yt.title}")
+                    yt.streams.filter(only_video=True).first().download(
+                        output_path="audio"
+                    )
+            print("audio baixado\n")
+    except (IndexError):
+        print_banner()
+    except (FileNotFoundError):
+        print("arquivo não encontrado")
+    except KeyboardInterrupt:
+        print("\nsaindo do programa ...")
+        sys.exit()
+
+
+if __name__ == "__main__":
+    main()
