e7ab807dcfd9de962455a75fea487e17a89d0fa4
Lucas Bispo <skaliboo1@gmail.com>
2022-12-22 16:08:21 -0300

    fix - login in plataform

--- uploadSelenium.py
+++ uploadSelenium.py
@@ -1,7 +1,25 @@
-from selenium.webdriver import Chrome
+from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+import os
+
+from dotenv import load_dotenv
 
 
 def upload_file(file_path):
+    load_dotenv()
     url = "https://anchor.fm/login"
-    UnicodeTranslateError
+    email = os.getenv("EMAIL")
+    passwd = os.getenv("PASSWORD")
+    drive = Firefox()
+    drive.get(url)
+    drive.find_element(by="id", value="email").send_keys(email)
+    drive.find_element(by="id", value="password").send_keys(passwd, Keys.ENTER)
+
+    sleep(10)
+    drive.close()
     
+
+if __name__ == "__main__":
+    upload_file("path/to/file")
