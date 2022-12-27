6c87d07b712c3de9f2c8cf2e90003c0a3525b270
Lucas Bispo <skaliboo1@gmail.com>
2022-12-27 15:45:50 -0300

    docs format

--- index.py
+++ index.py
@@ -14,14 +14,19 @@ def main():
                 for i, line in enumerate(file):
                     yt = YouTube(line)
                     print(f" {int(i) + 1} - Downloading {yt.title}")
-                    download = yt.streams.filter(only_audio=True).first().download(
-                        output_path="audio"
+                    download = (
+                        yt.streams.filter(only_audio=True)
+                        .first()
+                        .download(output_path="audio")
                     )
                     base, ext = os.path.splitext(download)
                     print(base)
                     print(ext)
                     os.rename(download, base + ".mp3")
-                    upload_file(f"{base}.mp3", {"title": yt.title, "description": yt.description})
+                    upload_file(
+                        f"{base}.mp3",
+                        {"title": yt.title, "description": yt.description},
+                    )
         print("Download complete")
         sys.exit()
     except (IndexError):
