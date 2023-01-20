62ee4a820fe917e97c9301ab5577dbd218a3c148
Lucas Bispo <skaliboo1@gmail.com>
2023-01-20 09:48:24 -0300

    rename file to improve semantics

--- verifyLastVideoInYoutube.py
+++ verifyLastVideoInYoutube.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import os
 from dotenv import load_dotenv
 import re
-
+from index import main
 load_dotenv()
 url = f"https://www.googleapis.com/youtube/v3/search?key={os.getenv('API_KEY')}&channelId={os.getenv('CHANNEL_ID')}&part=snippet,id&order=date&maxResults=1"
 response = requests.get(url)
@@ -16,6 +16,6 @@ if time.date() == datetime.now().date():
     title = data["items"][0]["snippet"]["title"]
     if re.search(r"estr", title, re.IGNORECASE):
         last_video_url = f"https://www.youtube.com/watch?v={data['items'][0]['id']['videoId']}"
-        print(last_video_url)
+        main(last_video_url)
 else:
     print("No video today")
