345e48ddd5b89922388e66231138b2a0e030991c
Lucas Bispo <skaliboo1@gmail.com>
2023-02-01 20:46:31 -0300

    add path to folder

--- init.sh
+++ init.sh
@@ -5,6 +5,13 @@ echo "Criando ambiente virtual..."
 python3 -m venv venv
 
 echo "criando pasta para logs caso não exista em /var/log/anchor"
+
+FOLDER_NAME="/var/log/anchor"
+
+if [ ! -d "$FOLDER_NAME" ]; then
+    mkdir $FOLDER_NAME
+fi
+
 echo "Ativando ambiente virtual..."
 source venv/bin/activate
 echo "Ambiente virtual ativado!"
