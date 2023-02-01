431b58809797f6a61821a6e2ee9f974a257d99ff
Lucas Bispo <skaliboo1@gmail.com>
2023-02-01 18:51:36 -0300

    update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#shellcheck source=/dev/null
 
 echo "Criando ambiente virtual..."
 python3 -m venv venv
@@ -8,7 +9,7 @@ source venv/bin/activate
 echo "Ambiente virtual ativado!"
 
 echo "Instalando dependencias..."
-pip install -r /home/flaco/workspace/downloads_youtube/requirements.txt
+pip install -r "${PWD}"/requirements.txt
 
 echo "Executando arquivo Python..."
-python3 /home/flaco/workspace/downloads_youtube/t.py
+python3 "${PWD}"/verifyLastVideoInYoutube.py
