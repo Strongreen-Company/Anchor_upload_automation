3bcfb08aec603c30a8af224eefd51cadd9791ceb
Lucas Bispo <skaliboo1@gmail.com>
2023-02-02 17:25:27 -0300

    update sacript init

--- init.sh
+++ init.sh
@@ -6,8 +6,14 @@ python3 -m venv venv
 
 echo "Criando pasta para armazenamento de logs em /log/anchor"
 
-FOLDER_NAME="log/anchor"
+FOLDER_NAME_PARENT="log"
+if [ ! -d "$FOLDER_NAME_PARENT" ]; then
+    sudo mkdir "$FOLDER_NAME_PARENT"
+    sudo chmod +755 "${FOLDER_NAME_PARENT}"
+    echo "Pasta pai criada com sucesso em ${FOLDER_NAME_PARENT} e permissões definidas."
+fi
 
+FOLDER_NAME="log/anchor"
 if [ ! -d "$FOLDER_NAME" ]; then
     sudo mkdir "$FOLDER_NAME"
     sudo chmod +755 "${FOLDER_NAME}"
@@ -24,5 +30,5 @@ echo "Instalando dependências listadas no arquivo requirements.txt..."
 pip install -r "${PWD}"/requirements.txt
 
 echo "Executando arquivo Python verifyLastVideoInYoutube.py..."
-python3 "${PWD}"/verifyLastVideoInYoutube.py
+sudo python"${PWD}"/verifyLastVideoInYoutube.py
 echo "Execução do script concluída."
