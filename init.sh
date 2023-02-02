38a67c4a51af689e72195df869491d6185fc54b0
Lucas Bispo <skaliboo1@gmail.com>
2023-02-02 16:57:07 -0300

    fix: update init

--- init.sh
+++ init.sh
@@ -4,22 +4,25 @@
 echo "Criando ambiente virtual..."
 python3 -m venv venv
 
-echo "criando pasta para logs caso não exista em /var/log/anchor"
+echo "Criando pasta para armazenamento de logs em /var/log/anchor"
 
-FOLDER_NAME="${PWD}/var/log/anchor"
+FOLDER_NAME="/var/log/anchor"
 
 if [ ! -d "$FOLDER_NAME" ]; then
-    mkdir "$FOLDER_NAME"
-    chmod +777 "${FOLDER_NAME}"
+    sudo mkdir "$FOLDER_NAME"
+    sudo chmod +755 "${FOLDER_NAME}"
+    echo "Pasta criada com sucesso em ${FOLDER_NAME} e permissões definidas."
+else
+    echo "Pasta já existente em ${FOLDER_NAME}."
 fi
 
 echo "Ativando ambiente virtual..."
 source venv/bin/activate
-echo "Ambiente virtual ativado!"
-sleep 5s
+echo "Ambiente virtual ativado com sucesso!"
 
-echo "Instalando dependencias..."
+echo "Instalando dependências listadas no arquivo requirements.txt..."
 pip install -r "${PWD}"/requirements.txt
 
-echo "Executando arquivo Python..."
+echo "Executando arquivo Python verifyLastVideoInYoutube.py..."
 python3 "${PWD}"/verifyLastVideoInYoutube.py
+echo "Execução do script concluída."
